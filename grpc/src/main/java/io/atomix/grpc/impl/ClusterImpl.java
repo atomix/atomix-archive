b6481664fa52b2070495c81b66f6226e08d22e0e
Jordan Halterman <jordan.halterman@gmail.com>
2019-03-29 00:00:15 -0700

    Implement cluster service.

---
+++ grpc/src/main/java/io/atomix/grpc/impl/ClusterImpl.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2019-present Open Networking Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.atomix.grpc.impl;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import com.google.protobuf.Empty;
+import io.atomix.cluster.ClusterMembershipEvent;
+import io.atomix.cluster.ClusterMembershipEventListener;
+import io.atomix.core.Atomix;
+import io.atomix.grpc.cluster.ClusterGrpc;
+import io.atomix.grpc.cluster.GetMemberRequest;
+import io.atomix.grpc.cluster.GetMemberResponse;
+import io.atomix.grpc.cluster.GetMembersResponse;
+import io.atomix.grpc.cluster.Member;
+import io.atomix.grpc.cluster.MemberEvent;
+import io.atomix.utils.net.Address;
+import io.grpc.stub.StreamObserver;
+
+/**
+ * gRPC cluster service implementation.
+ */
+public class ClusterImpl extends ClusterGrpc.ClusterImplBase {
+  private final Atomix atomix;
+
+  public ClusterImpl(Atomix atomix) {
+    this.atomix = atomix;
+  }
+
+  private Member toMember(io.atomix.cluster.Member member) {
+    return member == null ? null : Member.newBuilder()
+        .setId(member.id().id())
+        .setHost(member.address().host())
+        .setPort(member.address().port())
+        .setZoneId(member.zone())
+        .setRackId(member.rack())
+        .setHostId(member.host())
+        .build();
+  }
+
+  @Override
+  public void getLocalMember(Empty request, StreamObserver<Member> responseObserver) {
+    io.atomix.cluster.Member member = atomix.getMembershipService().getLocalMember();
+    responseObserver.onNext(toMember(member));
+    responseObserver.onCompleted();
+  }
+
+  @Override
+  public void getMembers(Empty request, StreamObserver<GetMembersResponse> responseObserver) {
+    Collection<io.atomix.cluster.Member> members = atomix.getMembershipService().getMembers();
+    responseObserver.onNext(GetMembersResponse.newBuilder()
+        .addAllMembers(members.stream()
+            .map(this::toMember)
+            .collect(Collectors.toSet()))
+        .build());
+    responseObserver.onCompleted();
+  }
+
+  @Override
+  public void getMember(GetMemberRequest request, StreamObserver<GetMemberResponse> responseObserver) {
+    io.atomix.cluster.Member member;
+    if (request.getId() != null) {
+      member = atomix.getMembershipService().getMember(request.getId());
+    } else if (request.getHost() != null) {
+      member = atomix.getMembershipService().getMember(Address.from(request.getHost(), request.getPort()));
+    } else {
+      member = null;
+    }
+    responseObserver.onNext(GetMemberResponse.newBuilder()
+        .setMember(toMember(member))
+        .build());
+    responseObserver.onCompleted();
+  }
+
+  @Override
+  public StreamObserver<Empty> listen(StreamObserver<MemberEvent> responseObserver) {
+    ClusterMembershipEventListener listener = e -> {
+      MemberEvent event = toEvent(e);
+      if (event != null) {
+        responseObserver.onNext(event);
+      }
+    };
+    atomix.getMembershipService().addListener(listener);
+
+    return new StreamObserver<Empty>() {
+      @Override
+      public void onNext(Empty value) {
+      }
+
+      @Override
+      public void onError(Throwable t) {
+        atomix.getMembershipService().removeListener(listener);
+        responseObserver.onCompleted();
+      }
+
+      @Override
+      public void onCompleted() {
+        atomix.getMembershipService().removeListener(listener);
+        responseObserver.onCompleted();
+      }
+    };
+  }
+
+  private MemberEvent toEvent(ClusterMembershipEvent event) {
+    switch (event.type()) {
+      case MEMBER_ADDED:
+        return MemberEvent.newBuilder()
+            .setType(MemberEvent.Type.ADDED)
+            .setMember(toMember(event.subject()))
+            .build();
+      case MEMBER_REMOVED:
+        return MemberEvent.newBuilder()
+            .setType(MemberEvent.Type.REMOVED)
+            .setMember(toMember(event.subject()))
+            .build();
+      default:
+        return null;
+    }
+  }
+}
---
+++ grpc/src/main/java/io/atomix/grpc/impl/NettyGrpcService.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2019-present Open Networking Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.atomix.grpc.impl;
+
+import java.util.concurrent.CompletableFuture;
+
+import io.atomix.core.Atomix;
+import io.atomix.grpc.GrpcService;
+import io.atomix.grpc.ManagedGrpcService;
+import io.atomix.utils.concurrent.Futures;
+import io.atomix.utils.net.Address;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+/**
+ * Netty gRPC service.
+ */
+public class NettyGrpcService implements ManagedGrpcService {
+  private final Atomix atomix;
+  private final Address address;
+  private Server server;
+
+  public NettyGrpcService(Atomix atomix, Address address) {
+    this.atomix = atomix;
+    this.address = address;
+  }
+
+  @Override
+  public Address address() {
+    return address;
+  }
+
+  @Override
+  public CompletableFuture<GrpcService> start() {
+    server = ServerBuilder.forPort(address.port())
+        .addService(new ClusterImpl(atomix))
+        .build();
+    try {
+      server.start();
+    } catch (Exception e) {
+      return Futures.exceptionalFuture(e);
+    }
+    return CompletableFuture.completedFuture(this);
+  }
+
+  @Override
+  public boolean isRunning() {
+    return !server.isShutdown();
+  }
+
+  @Override
+  public CompletableFuture<Void> stop() {
+    server.shutdown();
+    return CompletableFuture.completedFuture(null);
+  }
+
+  /**
+   * Netty gRPC service builder.
+   */
+  public static class Builder extends ManagedGrpcService.Builder {
+    @Override
+    public ManagedGrpcService build() {
+      return new NettyGrpcService(atomix, address);
+    }
+  }
+}
