59528a8d4ec5ba3d582e6fa386cc782d97120b7d
Jordan Halterman <jordan.halterman@gmail.com>
2019-04-08 16:31:58 -0700

    Use primitive executor in gRPC list service.

--- grpc/src/main/java/io/atomix/grpc/impl/ListServiceImpl.java
+++ grpc/src/main/java/io/atomix/grpc/impl/ListServiceImpl.java
@@ -16,15 +16,15 @@
 package io.atomix.grpc.impl;
 
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import com.google.protobuf.ByteString;
 import io.atomix.core.Atomix;
 import io.atomix.core.collection.CollectionEventListener;
 import io.atomix.core.list.AsyncDistributedList;
+import io.atomix.core.list.DistributedList;
+import io.atomix.core.list.DistributedListType;
 import io.atomix.grpc.list.AddRequest;
 import io.atomix.grpc.list.AddResponse;
 import io.atomix.grpc.list.ClearRequest;
@@ -40,60 +40,16 @@ import io.atomix.grpc.list.RemoveRequest;
 import io.atomix.grpc.list.RemoveResponse;
 import io.atomix.grpc.list.SizeRequest;
 import io.atomix.grpc.list.SizeResponse;
-import io.atomix.primitive.protocol.ProxyProtocol;
-import io.atomix.protocols.backup.MultiPrimaryProtocol;
-import io.atomix.protocols.log.DistributedLogProtocol;
-import io.atomix.protocols.raft.MultiRaftProtocol;
 import io.grpc.stub.StreamObserver;
 
 /**
  * List service implementation.
  */
 public class ListServiceImpl extends ListServiceGrpc.ListServiceImplBase {
-  private final Atomix atomix;
+  private final PrimitiveExecutor<DistributedList<byte[]>, AsyncDistributedList<byte[]>> executor;
 
   public ListServiceImpl(Atomix atomix) {
-    this.atomix = atomix;
-  }
-
-  private ProxyProtocol toProtocol(ListId id) {
-    if (id.hasRaft()) {
-      return MultiRaftProtocol.builder(id.getRaft().getGroup())
-          .build();
-    } else if (id.hasMultiPrimary()) {
-      return MultiPrimaryProtocol.builder(id.getMultiPrimary().getGroup())
-          .build();
-    } else if (id.hasLog()) {
-      return DistributedLogProtocol.builder(id.getLog().getGroup())
-          .build();
-    }
-    return null;
-  }
-
-  private CompletableFuture<AsyncDistributedList<byte[]>> getList(ListId id) {
-    return atomix.<byte[]>listBuilder(id.getName())
-        .withProtocol(toProtocol(id))
-        .getAsync()
-        .thenApply(list -> list.async());
-  }
-
-  private <T> void run(ListId id, Function<AsyncDistributedList<byte[]>, CompletableFuture<T>> function, StreamObserver<T> responseObserver) {
-    getList(id).whenComplete((list, getError) -> {
-      if (getError == null) {
-        function.apply(list).whenComplete((result, funcError) -> {
-          if (funcError == null) {
-            responseObserver.onNext(result);
-            responseObserver.onCompleted();
-          } else {
-            responseObserver.onError(funcError);
-            responseObserver.onCompleted();
-          }
-        });
-      } else {
-        responseObserver.onError(getError);
-        responseObserver.onCompleted();
-      }
-    });
+    this.executor = new PrimitiveExecutor<>(atomix, DistributedListType.instance(), DistributedList::async);
   }
 
   @Override
@@ -102,18 +58,20 @@ public class ListServiceImpl extends ListServiceGrpc.ListServiceImplBase {
       responseObserver.onNext(AddResponse.newBuilder().setAdded(false).build());
       responseObserver.onCompleted();
     } else if (request.getValuesCount() == 1) {
-      run(request.getId(), list -> list.add(request.getValues(0).toByteArray())
-          .thenApply(added -> AddResponse.newBuilder()
-              .setAdded(added)
-              .build()), responseObserver);
+      executor.execute(request, AddResponse::getDefaultInstance, responseObserver,
+          list -> list.add(request.getValues(0).toByteArray())
+              .thenApply(added -> AddResponse.newBuilder()
+                  .setAdded(added)
+                  .build()));
     } else {
-      run(request.getId(), list -> list.addAll(request.getValuesList()
-          .stream()
-          .map(ByteString::toByteArray)
-          .collect(Collectors.toSet()))
-          .thenApply(added -> AddResponse.newBuilder()
-              .setAdded(added)
-              .build()), responseObserver);
+      executor.execute(request, AddResponse::getDefaultInstance, responseObserver,
+          list -> list.addAll(request.getValuesList()
+              .stream()
+              .map(ByteString::toByteArray)
+              .collect(Collectors.toSet()))
+              .thenApply(added -> AddResponse.newBuilder()
+                  .setAdded(added)
+                  .build()));
     }
   }
 
@@ -123,18 +81,20 @@ public class ListServiceImpl extends ListServiceGrpc.ListServiceImplBase {
       responseObserver.onNext(RemoveResponse.newBuilder().setRemoved(false).build());
       responseObserver.onCompleted();
     } else if (request.getValuesCount() == 1) {
-      run(request.getId(), list -> list.remove(request.getValues(0).toByteArray())
-          .thenApply(removed -> RemoveResponse.newBuilder()
-              .setRemoved(removed)
-              .build()), responseObserver);
+      executor.execute(request, RemoveResponse::getDefaultInstance, responseObserver,
+          list -> list.remove(request.getValues(0).toByteArray())
+              .thenApply(removed -> RemoveResponse.newBuilder()
+                  .setRemoved(removed)
+                  .build()));
     } else {
-      run(request.getId(), list -> list.removeAll(request.getValuesList()
-          .stream()
-          .map(ByteString::toByteArray)
-          .collect(Collectors.toSet()))
-          .thenApply(removed -> RemoveResponse.newBuilder()
-              .setRemoved(removed)
-              .build()), responseObserver);
+      executor.execute(request, RemoveResponse::getDefaultInstance, responseObserver,
+          list -> list.removeAll(request.getValuesList()
+              .stream()
+              .map(ByteString::toByteArray)
+              .collect(Collectors.toSet()))
+              .thenApply(removed -> RemoveResponse.newBuilder()
+                  .setRemoved(removed)
+                  .build()));
     }
   }
 
@@ -144,35 +104,45 @@ public class ListServiceImpl extends ListServiceGrpc.ListServiceImplBase {
       responseObserver.onNext(ContainsResponse.newBuilder().setContains(false).build());
       responseObserver.onCompleted();
     } else if (request.getValuesCount() == 1) {
-      run(request.getId(), list -> list.contains(request.getValues(0).toByteArray())
-          .thenApply(contains -> ContainsResponse.newBuilder()
-              .setContains(contains)
-              .build()), responseObserver);
+      executor.execute(request, ContainsResponse::getDefaultInstance, responseObserver,
+          list -> list.contains(request.getValues(0).toByteArray())
+              .thenApply(contains -> ContainsResponse.newBuilder()
+                  .setContains(contains)
+                  .build()));
     } else {
-      run(request.getId(), list -> list.containsAll(request.getValuesList()
-          .stream()
-          .map(ByteString::toByteArray)
-          .collect(Collectors.toSet()))
-          .thenApply(contains -> ContainsResponse.newBuilder()
-              .setContains(contains)
-              .build()), responseObserver);
+      executor.execute(request, ContainsResponse::getDefaultInstance, responseObserver,
+          list -> list.containsAll(request.getValuesList()
+              .stream()
+              .map(ByteString::toByteArray)
+              .collect(Collectors.toSet()))
+              .thenApply(contains -> ContainsResponse.newBuilder()
+                  .setContains(contains)
+                  .build()));
     }
   }
 
   @Override
   public void get(GetRequest request, StreamObserver<GetResponse> responseObserver) {
-    run(request.getId(), list -> list.get(request.getIndex())
-        .thenApply(value -> GetResponse.newBuilder().setValue(ByteString.copyFrom(value)).build()), responseObserver);
+    executor.execute(request, GetResponse::getDefaultInstance, responseObserver,
+        list -> list.get(request.getIndex())
+            .thenApply(value -> GetResponse.newBuilder()
+                .setValue(ByteString.copyFrom(value))
+                .build()));
   }
 
   @Override
   public void size(SizeRequest request, StreamObserver<SizeResponse> responseObserver) {
-    run(request.getId(), list -> list.size().thenApply(size -> SizeResponse.newBuilder().setSize(size).build()), responseObserver);
+    executor.execute(request, SizeResponse::getDefaultInstance, responseObserver,
+        list -> list.size()
+            .thenApply(size -> SizeResponse.newBuilder()
+                .setSize(size)
+                .build()));
   }
 
   @Override
   public void clear(ClearRequest request, StreamObserver<ClearResponse> responseObserver) {
-    run(request.getId(), list -> list.clear().thenApply(v -> ClearResponse.newBuilder().build()), responseObserver);
+    executor.execute(request, ClearResponse::getDefaultInstance, responseObserver,
+        list -> list.clear().thenApply(v -> ClearResponse.newBuilder().build()));
   }
 
   @Override
@@ -181,37 +151,39 @@ public class ListServiceImpl extends ListServiceGrpc.ListServiceImplBase {
     return new StreamObserver<ListId>() {
       @Override
       public void onNext(ListId id) {
-        CollectionEventListener<byte[]> listener = event -> {
-          switch (event.type()) {
-            case ADD:
-              responseObserver.onNext(ListEvent.newBuilder()
-                  .setId(id)
-                  .setType(ListEvent.Type.ADDED)
-                  .setValue(ByteString.copyFrom(event.element()))
-                  .build());
-              break;
-            case REMOVE:
-              responseObserver.onNext(ListEvent.newBuilder()
-                  .setId(id)
-                  .setType(ListEvent.Type.REMOVED)
-                  .setValue(ByteString.copyFrom(event.element()))
-                  .build());
-              break;
-          }
-        };
-        listeners.put(id, listener);
-        getList(id).thenAccept(set -> set.addListener(listener));
+        if (executor.isValidId(id, ListEvent::getDefaultInstance, responseObserver)) {
+          CollectionEventListener<byte[]> listener = event -> {
+            switch (event.type()) {
+              case ADD:
+                responseObserver.onNext(ListEvent.newBuilder()
+                    .setId(id)
+                    .setType(ListEvent.Type.ADDED)
+                    .setValue(ByteString.copyFrom(event.element()))
+                    .build());
+                break;
+              case REMOVE:
+                responseObserver.onNext(ListEvent.newBuilder()
+                    .setId(id)
+                    .setType(ListEvent.Type.REMOVED)
+                    .setValue(ByteString.copyFrom(event.element()))
+                    .build());
+                break;
+            }
+          };
+          listeners.put(id, listener);
+          executor.getPrimitive(id).thenAccept(set -> set.addListener(listener));
+        }
       }
 
       @Override
       public void onError(Throwable t) {
-        listeners.forEach((id, listener) -> getList(id).thenAccept(list -> list.removeListener(listener)));
+        listeners.forEach((id, listener) -> executor.getPrimitive(id).thenAccept(list -> list.removeListener(listener)));
         responseObserver.onCompleted();
       }
 
       @Override
       public void onCompleted() {
-        listeners.forEach((id, listener) -> getList(id).thenAccept(list -> list.removeListener(listener)));
+        listeners.forEach((id, listener) -> executor.getPrimitive(id).thenAccept(list -> list.removeListener(listener)));
         responseObserver.onCompleted();
       }
     };
