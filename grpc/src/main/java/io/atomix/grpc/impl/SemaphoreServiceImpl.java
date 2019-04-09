54c304c95edaeae15e85dd58f9a159a0fae4fb0c
Jordan Halterman <jordan.halterman@gmail.com>
2019-04-08 17:18:09 -0700

    Use primitive executor in gRPC semaphore service.

--- grpc/src/main/java/io/atomix/grpc/impl/SemaphoreServiceImpl.java
+++ grpc/src/main/java/io/atomix/grpc/impl/SemaphoreServiceImpl.java
@@ -15,84 +15,40 @@
  */
 package io.atomix.grpc.impl;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.function.Function;
-
 import io.atomix.core.Atomix;
 import io.atomix.core.semaphore.AsyncAtomicSemaphore;
+import io.atomix.core.semaphore.AtomicSemaphore;
+import io.atomix.core.semaphore.AtomicSemaphoreType;
 import io.atomix.grpc.semaphore.AcquireRequest;
 import io.atomix.grpc.semaphore.AcquireResponse;
 import io.atomix.grpc.semaphore.ReleaseRequest;
 import io.atomix.grpc.semaphore.ReleaseResponse;
-import io.atomix.grpc.semaphore.SemaphoreId;
 import io.atomix.grpc.semaphore.SemaphoreServiceGrpc;
-import io.atomix.primitive.protocol.ProxyProtocol;
-import io.atomix.protocols.backup.MultiPrimaryProtocol;
-import io.atomix.protocols.log.DistributedLogProtocol;
-import io.atomix.protocols.raft.MultiRaftProtocol;
 import io.grpc.stub.StreamObserver;
 
 /**
  * Semaphore service implementation.
  */
 public class SemaphoreServiceImpl extends SemaphoreServiceGrpc.SemaphoreServiceImplBase {
-  private final Atomix atomix;
+  private final PrimitiveExecutor<AtomicSemaphore, AsyncAtomicSemaphore> executor;
 
   public SemaphoreServiceImpl(Atomix atomix) {
-    this.atomix = atomix;
-  }
-
-  private ProxyProtocol toProtocol(SemaphoreId id) {
-    if (id.hasRaft()) {
-      return MultiRaftProtocol.builder(id.getRaft().getGroup())
-          .build();
-    } else if (id.hasMultiPrimary()) {
-      return MultiPrimaryProtocol.builder(id.getMultiPrimary().getGroup())
-          .build();
-    } else if (id.hasLog()) {
-      return DistributedLogProtocol.builder(id.getLog().getGroup())
-          .build();
-    }
-    return null;
-  }
-
-  private CompletableFuture<AsyncAtomicSemaphore> getSemaphore(SemaphoreId id) {
-    return atomix.atomicSemaphoreBuilder(id.getName())
-        .withProtocol(toProtocol(id))
-        .getAsync()
-        .thenApply(semaphore -> semaphore.async());
-  }
-
-  private <T> void run(SemaphoreId id, Function<AsyncAtomicSemaphore, CompletableFuture<T>> function, StreamObserver<T> responseObserver) {
-    getSemaphore(id).whenComplete((semaphore, getError) -> {
-      if (getError == null) {
-        function.apply(semaphore).whenComplete((result, funcError) -> {
-          if (funcError == null) {
-            responseObserver.onNext(result);
-            responseObserver.onCompleted();
-          } else {
-            responseObserver.onError(funcError);
-            responseObserver.onCompleted();
-          }
-        });
-      } else {
-        responseObserver.onError(getError);
-        responseObserver.onCompleted();
-      }
-    });
+    this.executor = new PrimitiveExecutor<>(atomix, AtomicSemaphoreType.instance(), AtomicSemaphore::async);
   }
 
   @Override
   public void acquire(AcquireRequest request, StreamObserver<AcquireResponse> responseObserver) {
-    run(request.getId(), semaphore -> semaphore.acquire(request.getPermits() > 0 ? request.getPermits() : 1)
+    executor.execute(request, AcquireResponse::getDefaultInstance, responseObserver,
+        semaphore -> semaphore.acquire(request.getPermits() > 0 ? request.getPermits() : 1)
         .thenApply(version -> AcquireResponse.newBuilder()
             .setVersion(version.value())
-            .build()), responseObserver);
+            .build()));
   }
 
   @Override
   public void release(ReleaseRequest request, StreamObserver<ReleaseResponse> responseObserver) {
-    run(request.getId(), semaphore -> semaphore.release(request.getPermits() > 0 ? request.getPermits() : 1)
-        .thenApply(version -> ReleaseResponse.newBuilder().build()), responseObserver);
+    executor.execute(request, ReleaseResponse::getDefaultInstance, responseObserver,
+        semaphore -> semaphore.release(request.getPermits() > 0 ? request.getPermits() : 1)
+            .thenApply(version -> ReleaseResponse.newBuilder().build()));
   }
 }
